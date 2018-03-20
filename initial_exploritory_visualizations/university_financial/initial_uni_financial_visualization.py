e2c92dc1215f527bc9863895107fb1cfed7af112
Vincent Wahl <vincentwahl@dhcp-wifi-8021x-155-41-16-217.bu.edu>
2018-03-20 17:56:10 -0400

    graphed sum, mean, mean non zero of research allocations

--- initial_exploritory_visualizations/university_financial/initial_uni_financial_visualization.py
+++ initial_exploritory_visualizations/university_financial/initial_uni_financial_visualization.py
@@ -13,25 +13,47 @@ cols_research = ['0102_RESEARCH','0203_RESEARCH','0304_RESEARCH','0405_RESEARCH'
 
 research = df[cols_research]
 
-research_sum_all = df.sum().tolist()
+research_sum_all = research.sum().tolist()
 
-research_mean_all = [tot for tot in research_sum_all]
+research_mean_all = [tot/research.shape[0] for tot in research_sum_all]
 
-#research_mean_non_zero = df
+research_n_non_zero = research.astype(bool).sum(axis=0)
+
+research_mean_non_zero = [research_sum_all[i]/research_n_non_zero[i] for i in range(len(research_sum_all))]
 
 bins = ['01-02','02-03','03-04','04-05','05-06',
 	 '06-07','07-08','08-09','09-10','10-11',
 	 '11-12','12-13','13-14','14-15','15-16']
 
-plt.grid()
-plt.bar(bins, research_mean_all, align='center')
 
-# labels
-plt.title('Total amount of USD allocated to research accross post-secondary US educational institutions')
+plt.grid()
+plt.bar(bins, research_sum_all, align='center')
+plt.title('Total amount of USD allocated to research\naccross post-secondary US educational institutions')
 plt.xlabel('School year')
 plt.ylabel('USD allocated to research')
-
 plt.show()
 
-print(research_mean_all)
-print()
+plt.grid()
+plt.bar(bins, research_mean_all, align='center')
+plt.title('Mean amount of USD allocated to research accross\nall post-secondary US educational institutions')
+plt.xlabel('School year')
+plt.ylabel('USD allocated to research')
+plt.show()
+
+plt.grid()
+plt.bar(bins, research_mean_non_zero, align='center')
+plt.title('Mean amount of USD allocated to research accross\npost-secondary US educational institutions doing research')
+plt.xlabel('School year')
+plt.ylabel('USD allocated to research')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
